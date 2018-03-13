fc386b9dba95f7d0fdecad3000d3bb4ce5be876e
Patryk Kopeć <Margok1234@gmail.com>
2018-03-13 23:52:11 +0100

    TG06 Created PatrolRoute component that replaces functionallity of PatrollingGuard class.

--- Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
@@ -2,25 +2,33 @@
 
 #include "ChooseNextWaypoint.h"
 #include "AIController.h"
-#include "PatrollingGuard.h"
+#include "PatrolRoute.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
-	// Get patrol points
-	
-	auto AIController = OwnerComp.GetAIOwner();
-	auto ControlledPawn = AIController->GetPawn();
-	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
-	auto PatrolPoints = PatrollingGuard->GetPatrollingPoints();
-	
+
+
+	// Get patrol route
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRouteComponent = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+
+	if (!ensure(PatrolRouteComponent)) { return EBTNodeResult::Failed; }
+
+	auto PatrolRoute = PatrolRouteComponent->GetPatrollingPoints();
+	if (PatrolRoute.Num() == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("A guard is missing patrol points"));
+		return EBTNodeResult::Failed;
+	}
+
 	// Set next waypoint
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
 	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName,
-		PatrolPoints[Index]);
-	
+		PatrolRoute[Index]);
+
 	// Cycle the index
-	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	auto NextIndex = (Index + 1) % PatrolRoute.Num();
 	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
 
 	UE_LOG(LogTemp, Warning, TEXT("Waypoint index: %i"), Index);
