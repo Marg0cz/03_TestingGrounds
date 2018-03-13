f21928dbb89cb4f8785e567e5b675e299179ae82
Patryk Kopeć <Margok1234@gmail.com>
2018-03-13 22:13:41 +0100

    TG05 Moved PatrollingSystem to C++.

---
+++ Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ChooseNextWaypoint.h"
+#include "AIController.h"
+#include "PatrollingGuard.h"
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
+{
+	// Get patrol points
+	
+	auto AIController = OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+	auto PatrolPoints = PatrollingGuard->GetPatrollingPoints();
+	
+	// Set next waypoint
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName,
+		PatrolPoints[Index]);
+	
+	// Cycle the index
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+
+	UE_LOG(LogTemp, Warning, TEXT("Waypoint index: %i"), Index);
+
+	return EBTNodeResult::Succeeded;
+}
